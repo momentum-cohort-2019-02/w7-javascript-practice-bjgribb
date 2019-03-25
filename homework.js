73fb2139e39417010db42046e46591cfb4769159
bjgribb <bjgribb@gmail.com>
2019-03-25 13:08:11 -0400

    answers 4-7

--- homework.js
+++ homework.js
@@ -17,17 +17,42 @@ function greeting(name) {
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
+function isOdd(number) {
+    if (number % 2 === 1 || number % 2 === -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+
+function isEven(number){
+    if (number % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
 
+function fahrenheitToCelsius(tempFarenheit) {
+    return (tempFarenheit - 32) * (5/9)
+}
+
+
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+
+function celsiusToFahrenheit(tempCelsius) {
+    return (tempCelsius * 9/5) + 32
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -35,6 +60,10 @@ function greeting(name) {
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+
+function fahrenheitToKelvin(tempFarenheit) {
+    return fahrenheitToCelsius(tempFarenheit) + 273.15
+}
 
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
